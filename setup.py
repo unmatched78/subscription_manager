1233e62714bf4742dfd391b172f29ab1d6baee47
unmatched78 <marvinavi24@gmail.com>
2025-06-26 10:49:02 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="subscription_manager",
+    version="0.1.0",
+    description="Framework-agnostic subscription & trial management",
+    author="Your Name",
+    author_email="you@example.com",
+    packages=find_packages(),
+    install_requires=[
+        "SQLAlchemy>=2.0",
+        "psycopg2-binary>=2.9"
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
